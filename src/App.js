4b779c5c14e85040659d12a45cb41a45d4da712f
nada <nadas0310@gmail.com>
2024-03-01 18:52:16 +0200

    task1

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+// App.js
 import React from 'react';
-import './App.css';
-import ImageSlider from './ImageSlider';
+import ImageSlider from './components/ImageSlider';
 
-function App() {
+const App = () => {
+  const imageNames = [
+    'image1.jpg',
+    'image2.jpg',
+    'image3.jpg',
+    // Add more image names here as needed
+  ];
+
+  const images = imageNames.map(imageName => imageName); // No need to include PUBLIC_URL
+
   return (
-    <div className="App">
-      <ImageSlider />
+    <div>
+      <h1>Image Slider Example</h1>
+      <ImageSlider images={images} />
     </div>
   );
-}
+};
 
 export default App;
+
+
